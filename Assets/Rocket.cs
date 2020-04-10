7c508441f64bb332429e949c4cf1cc819b8a0e5e
kstapes <46645798+kstapes@users.noreply.github.com>
2020-04-10 13:23:52 +0100

    Rotation Speed
    Adjusted Rotation speed

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -18,34 +18,44 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
-            if(!audioSource.isPlaying)
+            if (!audioSource.isPlaying)
             {
                 audioSource.Play();
-            } 
-            
+            }
+
         }
         else
         {
             audioSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+
+        float rcsThrust = 100f;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
-        
-        
+
+        rigidBody.freezeRotation = false; //resume physics control of rotation
     }
+
 }
