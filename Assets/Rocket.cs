b0cfbf0ba50ac7e754d97de59aa25f1ed090b51a
kstapes <46645798+kstapes@users.noreply.github.com>
2020-04-16 15:46:52 +0100

    Level States
    added states to levels and disabled input when user dies

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 100f;
@@ -10,6 +8,10 @@ public class Rocket : MonoBehaviour
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
+
+    //bool ButtonsEnabled = true; first pass at disabling buttons when player dies
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +19,15 @@ public class Rocket : MonoBehaviour
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
-
+        
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
     }
     void OnCollisionEnter(Collision collision)
     {
@@ -30,17 +35,31 @@ public class Rocket : MonoBehaviour
         {
             case "Friendly":
                 //do nothing if friendly
-                print("OK");
+                break;
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextScene", 1f);
                 break;
             default:
-                print("Dead");
+                state = State.Dying;
+                //ButtonsEnabled = false;
+                Invoke("LoadFirstScene", 1f);
                 break;
         }
+    }
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+        //ButtonsEnabled = true;
+    }
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
     }
 
     private void Thrust()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space)) //&& ButtonsEnabled == true
         {
             float thrustThisFrame = mainThrust * Time.deltaTime;
             rigidBody.AddRelativeForce(Vector3.up * mainThrust);
@@ -62,11 +81,11 @@ public class Rocket : MonoBehaviour
 
         float rotationThisFrame = rcsThrust * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A)) //&& ButtonsEnabled == true
         {
             transform.Rotate(Vector3.forward * rotationThisFrame);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))//&& ButtonsEnabled == true
         {
             transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
