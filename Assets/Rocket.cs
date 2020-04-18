56766a9b6c124129eb40b87f2d4e519c40c2727d
kstapes <46645798+kstapes@users.noreply.github.com>
2020-04-18 21:22:04 +0100

    Linked Particles
    Added Particles to unity and linked in Rocket.cs

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,51 +1,59 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class Rocket : MonoBehaviour
 {
-    [SerializeField] float rcsThrust = 100f;
-    [SerializeField] float mainThrust = 10f;
-    [SerializeField] AudioClip mainEngine;
-    [SerializeField] AudioClip success;
-    [SerializeField] AudioClip death;
+    [SerializeField] private float rcsThrust = 100f;
+    [SerializeField] private float mainThrust = 10f;
+    [SerializeField] private AudioClip mainEngine;
+    [SerializeField] private AudioClip success;
+    [SerializeField] private AudioClip death;
 
-    Rigidbody rigidBody;
-    AudioSource audioSource;
-    enum State { Alive, Dying, Transcending }
-    State state = State.Alive;
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
+
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+    private enum State
+    { Alive, Dying, Transcending }
+
+    private State state = State.Alive;
 
     //bool ButtonsEnabled = true; first pass at disabling buttons when player dies
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
-        
     }
-        
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        //todo stop sound on death
         if (state == State.Alive)
         {
             RespondToThrustInput();
             RespondToRotateInput();
         }
     }
-    void OnCollisionEnter(Collision collision)
+
+    private void OnCollisionEnter(Collision collision)
     {
-        if(state != State.Alive){return;}
+        if (state != State.Alive) { return; }
 
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 //do nothing if friendly
                 break;
+
             case "Finish":
                 StartSuccess();
                 break;
+
             default:
                 StartDeath();
                 break;
@@ -74,11 +82,10 @@ public class Rocket : MonoBehaviour
         SceneManager.LoadScene(0);
         //ButtonsEnabled = true;
     }
+
     private void LoadNextScene()
     {
-
         SceneManager.LoadScene(1);
-       
     }
 
     private void RespondToThrustInput()
@@ -105,7 +112,7 @@ public class Rocket : MonoBehaviour
 
     private void RespondToRotateInput()
     {
-        rigidBody.freezeRotation = true; //take manual control of rotation 
+        rigidBody.freezeRotation = true; //take manual control of rotation
         float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A)) //&& ButtonsEnabled == true
@@ -119,6 +126,4 @@ public class Rocket : MonoBehaviour
 
         rigidBody.freezeRotation = false; //resume physics control of rotation
     }
-
-   
 }
